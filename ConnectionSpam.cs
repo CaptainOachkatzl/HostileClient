74da9a71cf0706912543ab83e05208de179e9487
David Hofer <dave.hofer@hotmail.com>
2018-07-10 18:17:35 +0200

    split up connection spam modes

--- ConnectionSpam.cs
+++ ConnectionSpam.cs
@@ -11,6 +11,7 @@ namespace HostileClient
         public EndPoint Target { get; set; }
         public int Count { get; set; } = 1;
         public Logger Logger { get; set; } = new NoLog();
+        protected ConnectionInterface Connection { get; set; }
 
         public ConnectionSpam()
         {
@@ -25,12 +26,16 @@ namespace HostileClient
                 Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 socket.Connect(Target);
 
-                IConnectionCrypto Crypto = new ECOpenSSLCrypto(true);
-                TCPPacketConnection connection = new TCPPacketConnection(socket);
-                connection.Logger = Logger;
-                connection.InitializeCrypto(Crypto);
-                connection.Disconnect();
+                PostConnectActions(socket);
+
+                if(Connection != null && Connection.Connected)
+                    Connection.Disconnect();
             }
+        }
+
+        protected virtual void PostConnectActions(Socket socket)
+        {
+
         }
     }
 }
---
+++ CryptoHandshakeSpam.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using XSLibrary.Cryptography.ConnectionCryptos;
+using XSLibrary.Network.Connections;
+using XSLibrary.Utility;
+
+namespace HostileClient
+{
+    class CryptoHandshakeSpam : ConnectionSpam
+    {
+        public CryptoHandshakeSpam()
+        {
+        }
+
+        protected override void PostConnectActions(Socket socket)
+        {
+            IConnectionCrypto Crypto = new ECCrypto(true);
+            TCPPacketConnection connection = new TCPPacketConnection(socket);
+            connection.Logger = Logger;
+            connection.InitializeCrypto(Crypto);
+        }
+    }
+}
