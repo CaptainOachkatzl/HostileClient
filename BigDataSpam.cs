015f2c9544388f88427d284b56f88583170edb37
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-10 23:27:07 +0200

    now sending encrypted 16MB in 1KB chunks from 2 threads

--- BigDataSpam.cs
+++ BigDataSpam.cs
@@ -1,18 +1,41 @@
-﻿using XSLibrary.Cryptography.ConnectionCryptos;
+﻿using System.Threading;
+using XSLibrary.Cryptography.ConnectionCryptos;
 
 namespace HostileClient
 {
     class BigDataSpam : PacketSpam
     {
+        byte symbol = 0x55;
+
+        byte[] Data1;
+        byte[] Data2;
         public BigDataSpam()
         {
-            Length = 4096;
+            Length = 16 * 1024 * 1024;
         }
 
         protected override void InitActions()
         {
+            symbol = (byte)'A';
+            Data1 = CreateData(Length);
+
+            symbol = (byte)'B';
+            Data2 = CreateData(Length);
+
             Connection.MaxPackageSendSize = 1024;
-            //Connection.InitializeCrypto(new ECCrypto(true));
+            Connection.InitializeCrypto(new ECCrypto(true));
+        }
+
+        protected override void ExecutionActions()
+        {
+            Thread thread1 = new Thread(() => Connection.Send(Data1));
+            Thread thread2 = new Thread(() => Connection.Send(Data2));
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
         }
 
         protected override byte[] CreateData(int length)
@@ -20,7 +43,7 @@ namespace HostileClient
             byte[] data = new byte[length];
             for (int i = 0; i < length; i++)
             {
-                data[i] = (byte)(i % 256);
+                data[i] = symbol;
             }
 
             return data;
